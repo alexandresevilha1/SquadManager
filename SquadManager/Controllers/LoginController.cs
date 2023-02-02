58c8dc28c182fa65b252f085b85d4dcf23ea83e8
alexandresevilha1 <Alexandre.sevilha1@gmail.com>
2023-02-02 12:16:59 -0300

    Adicionar arquivos de projeto.

---
+++ SquadManager/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using SquadManager.Models;
+using SquadManager.Models.Validator;
+
+namespace SquadManager.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            UserViewModel user = new UserViewModel();
+
+            user.Email = "alexandre";
+
+            UserValidator validator = new UserValidator();
+            
+            ValidationResult results = validator.Validate(user);
+
+            if (!results.IsValid) 
+            {
+
+                foreach (var failure in results.Errors) 
+                {
+
+                    Console.WriteLine("Property " + failure.PropertyName + " failed validation. Error was: " + failure.ErrorMessage);
+
+                }
+
+            }
+
+            return View("index", user);
+
+        }
+
+        [HttpPost]
+
+        public IActionResult Test(UserViewModel user)
+        {
+        
+            user.Email= "email enviado";
+
+            return View("index", user);
+
+        }
+    }
+}
---
+++ SquadManager/Models/Validator/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace SquadManager.Models.Validator
+{
+    public class UserValidator : AbstractValidator<UserViewModel>
+    {
+
+        public UserValidator() 
+        {
+
+            RuleFor( user => user.Email).NotNull().WithMessage("E-mail vazio");
+            RuleFor(user => user.Email).EmailAddress().WithMessage("O e-mail é invalido");  
+            RuleFor( user => user.Password).NotNull().WithMessage("Digite a senha");  
+        }
+
+    }
+}
